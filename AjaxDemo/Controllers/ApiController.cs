07db3813b927cef976505fc2584e6e652203c2de
gimmywu <113874913+gimmywu@users.noreply.github.com>
2023-01-05 17:04:35 +0800

    ajax傳姓名錯誤

--- AjaxDemo/Controllers/ApiController.cs
+++ AjaxDemo/Controllers/ApiController.cs
@@ -6,9 +6,15 @@ namespace AjaxDemo.Controllers
 {
     public class ApiController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string name)
         {
-            return Content("Hello, ajax 吳","text/plain",Encoding.UTF8);
+            return Content("Hello, Ajax", "text/plain", Encoding.UTF8);
+            if (string.IsNullOrEmpty(name))
+            {
+                name = "Ajax";
+            }
+           
+            return Content($"Hello, {name}", "text/plain", Encoding.UTF8);
         } //content的參數有三種，1.字串或數字，2.資料型別，3.用什麼編碼，如果字串裡面有中文，但沒有設utf8，網頁會出現亂碼
     }
 }
